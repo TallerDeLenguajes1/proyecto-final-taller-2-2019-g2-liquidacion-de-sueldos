998a2552d02f134df6ef7d634b13e4f102222645
unknown <agustindelgado555@gmail.com>
2019-11-03 12:41:24 -0300

    En proceso

--- Entidades/Persona.cs
+++ Entidades/Persona.cs
@@ -8,6 +8,42 @@ namespace Entidades
 {
     public class Persona
     {
+        int legajo;
+        string nombres;
+        string apellidos;
+        string documento;
+        DateTime fechaNacimiento;
+        string sexo;
+        string baja;
+        
+        public int Legajo { get => legajo; set => legajo = value; }
+        public string Nombres { get => nombres; set => nombres = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Documento { get => documento; set => documento = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string Baja { get => baja; set => baja = value; }
+
+        public Persona() { }//constructor por defecto
+        public Persona(int Legajo,string Nombres ,string Apellidos,string Documento,DateTime FechaNacimiento,string Sexo,string Baja)//constructor por parametros
+        {
+            Legajo = this.legajo;
+            Nombres = this.nombres;
+            Apellidos = this.apellidos;
+            Documento = this.documento;
+            FechaNacimiento = this.fechaNacimiento;
+            Sexo = this.sexo;
+            Baja = this.baja;
+        }
+     
+        public override string ToString()
+        {
+            return legajo + " - " + nombres + " " + apellidos + "  " + documento + " "+ fechaNacimiento+""+sexo+""+baja;
+        }
+        public string ConcatenarDatos()
+        {
+            return legajo + ";" + nombres + ";" + apellidos + ";" + documento + ";" + fechaNacimiento+";"+sexo+""+baja;
+        }
 
     }
 }
