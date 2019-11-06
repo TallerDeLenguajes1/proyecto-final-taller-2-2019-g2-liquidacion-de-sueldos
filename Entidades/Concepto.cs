4b94099377c62251660f0fd0c54af7016dca9fa2
Benjamin Cortes <cortesjpb@gmail.com>
2019-11-05 21:12:45 -0300

    Clase Concepto creada

--- Entidades/Concepto.cs
+++ Entidades/Concepto.cs
@@ -8,6 +8,25 @@ namespace Entidades
 {
     class Concepto
     {
+        int idconcepto;
+        string concept;
+        float monto;
         public Concepto() { }    //Constructor por defecto
+
+        public Concepto(int idconcepto, string concept, float monto)
+        {
+            this.idconcepto = idconcepto;
+            this.concept = concept;
+            this.monto = monto;
+        }
+
+        public int Idconcepto { get => idconcepto; set => idconcepto = value; }
+        public string Concept { get => concept; set => concept = value; }
+        public float Monto { get => monto; set => monto = value; }
+
+        public override string ToString()
+        {
+            return idconcepto + "," + concept + "," + monto;
+        }
     }
 }
