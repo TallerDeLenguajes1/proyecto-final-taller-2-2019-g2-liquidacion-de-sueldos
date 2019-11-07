ad278a4aa7a147033b442422c032789bfe95b39e
Benjamin Cortes <cortesjpb@gmail.com>
2019-11-07 15:54:03 -0300

    TipoConcepto y Concepto actualizados

--- Entidades/Concepto.cs
+++ Entidades/Concepto.cs
@@ -8,25 +8,33 @@ namespace Entidades
 {
     public class Concepto
     {
-        int idconcepto;
-        string concept;
-        float monto;
+        private int idConcepto;
+        private int idTipoConcepto;
+        private int idrs;
+        private int legajo;
+        private float monto;
+        private int cantidad;
         public Concepto() { }    //Constructor por defecto
-
-        public Concepto(int idconcepto, string concept, float monto)
+        public Concepto(int idConcepto, int idTipoConcepto, int idrs, int legajo, float monto, int cantidad)
         {
-            this.idconcepto = idconcepto;
-            this.concept = concept;
+            this.idConcepto = idConcepto;
+            this.idTipoConcepto = idTipoConcepto;
+            this.idrs = idrs;
+            this.legajo = legajo;
             this.monto = monto;
+            this.cantidad = cantidad;
         }
 
-        public int Idconcepto { get => idconcepto; set => idconcepto = value; }
-        public string Concept { get => concept; set => concept = value; }
+        public int IdConcepto { get => idConcepto; set => idConcepto = value; }
+        public int IdTipoConcepto { get => idTipoConcepto; set => idTipoConcepto = value; }
+        public int Idrs { get => idrs; set => idrs = value; }
+        public int Legajo { get => legajo; set => legajo = value; }
         public float Monto { get => monto; set => monto = value; }
-
+        public int Cantidad { get => cantidad; set => cantidad = value; }
         public override string ToString()
         {
-            return idconcepto + "," + concept + "," + monto;
+            return idConcepto + "," + idTipoConcepto + "," + idrs + "," + legajo + "," + monto + "," + cantidad;
         }
+
     }
 }
--- Entidades/TipoConcepto.cs
+++ Entidades/TipoConcepto.cs
@@ -8,7 +8,25 @@ namespace Entidades
 {
     public class TipoConcepto
     {
+        int idTipoConcepto;
+        string concepto;
+        float monto;
+        public TipoConcepto() { }    //Constructor por defecto
+        public TipoConcepto(int idTipoConcepto, string concepto, float monto)
+        {
+            this.idTipoConcepto = idTipoConcepto;
+            this.concepto = concepto;
+            this.monto = monto;
+        }
 
+        public int IdTipoConcepto { get => idTipoConcepto; set => idTipoConcepto = value; }
+        public string Concepto { get => concepto; set => concepto = value; }
+        public float Monto { get => monto; set => monto = value; }
+
+        public override string ToString()
+        {
+            return idTipoConcepto + "," + concepto + "," + monto;
+        }
     }
 
 }
