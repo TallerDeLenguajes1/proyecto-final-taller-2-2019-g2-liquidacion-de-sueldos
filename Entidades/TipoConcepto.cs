999c4400c710a98f1672429cbb3a1da183e8da9d
Benjamin Cortes <cortesjpb@gmail.com>
2019-11-07 15:36:27 -0300

    Merged master con benjamin

--- Entidades/TipoConcepto.cs
+++ Entidades/TipoConcepto.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-<<<<<<< HEAD:Entidades/TipoConcepto.cs
     public class TipoConcepto
-=======
-    public class TipoCargo
->>>>>>> 8cf545d... Corrección de nombres de clases:Entidades/TipoCargo.cs
     {
+
     }
+
 }
